016de00d902dbbf57c41232d4fabd8169ef0cfd5
Johan Dahlin <johan@flumotion.com>
2008-07-24 09:36:46 +0000

    2008-07-24  Johan Dahlin  <johan@gnome.org>
        * flumotion/component/dvb/wizard_gtk.py:
        Clean up the wizard a bit further, to make the code closer
        to the other pages.

--- flumotion/component/dvb/wizard_gtk.py
+++ flumotion/component/dvb/wizard_gtk.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004,2005 Fluendo, S.L. (www.fluendo.com). All rights reserved.
+# Copyright (C) 2008 Fluendo, S.L. (www.fluendo.com). All rights reserved.
 
 # This file may be distributed and/or modified under the terms of
 # the GNU General Public License version 2 as published by
@@ -36,6 +36,17 @@ T_ = gettexter('flumotion')
 _ = gettext.gettext
 
 
+# for kiwi ObjectList
+class DVBChannel:
+    def __init__(self, sid, chandata):
+        self.service_id = sid
+        self.name = chandata.get('name', 'Unknown')
+        self._chandata = chandata
+
+    def getTransportStreamId(self):
+        return self._chandata["transport-stream-id"]
+
+
 class DVBProducer(AudioProducer, VideoProducer):
     componentType = 'dvb-producer'
     def __init__(self):
@@ -52,6 +63,8 @@ class DVBProducer(AudioProducer, VideoProducer):
         self.properties.framerate = 12.5
         self.properties.has_video = True
 
+    # Component
+
     def getFeederName(self, component):
         if isinstance(component, AudioEncoder):
             return 'audio'
@@ -61,52 +74,48 @@ class DVBProducer(AudioProducer, VideoProducer):
         else:
             raise AssertionError
 
+    # Public API
+
     def setTuningInformation(self, tsid):
         ts = self.transportStreams[tsid]
         self.properties.frequency = ts["frequency"]
-        if self.properties.dvb_type == 'T':
-            modulation=""
-            if ts["constellation"].startswith("QAM"):
-                modulation = int(ts["constellation"][3:])
-            elif ts["constellation"] == "reserved":
-                modulation = 64
-            else:
-                #modulation = ts["constellation"]
-                modulation = 64
-            self.properties.modulation = modulation
-            for param in ["code-rate-hp", "code-rate-lp"]:
-                if ts[param] == "reserved":
-                    ts[param] = "NONE"
+        dvbType = self.properties.dvb_type
+        if dvbType == 'T':
             self.properties.bandwidth = ts["bandwidth"]
-            self.properties.code_rate_hp = ts["code-rate-hp"]
-            self.properties.code_rate_lp = ts["code-rate-lp"]
-            self.properties.trans_mode = int(ts["transmission-mode"][0]) 
             self.properties.guard = ts["guard-interval"]
             self.properties.hierarchy = ts["hierarchy"]
-            self.properties.modulation = modulation
-        elif self.adaptertype == "DVB-S":
-            if ts["inner-fec"] == "reserved" or \
-               ts["inner-fec"] == "none":
-                ts["inner-fec"] = "NONE"
+            self.properties.trans_mode = int(ts["transmission-mode"][0])
+            self.properties.code_rate_hp = self._parseCodeRate(ts["code-rate-hp"])
+            self.properties.code_rate_lp = self._parseCodeRate(ts["code-rate-lp"])
+        elif dvbType == "S":
             self.properties.polarity = ts["polarization"]
-            self.properties.symbol_rate = ts["symbol-rate"]
-            self.properties.code_rate_hp = ts["inner-fec"]
-        elif self.adaptertype == "DVB-C":
-            if ts["inner-fec"] == "reserved" or \
-               ts["inner-fec"] == "none":
-                ts["inner-fec"] = "NONE"
-
+        elif dvbType == "C":
             self.properties.inversion = "AUTO"
+
+        if dvbType != 'S':
+            self.properties.modulation = self._parseModulation(ts["constellation"])
+
+        if dvbType != 'T':
             self.properties.symbol_rate = ts["symbol-rate"]
-            self.properties.code_rate_hp = ts["inner-fec"]
-            modulation=""
-            if ts["modulation"].startswith("QAM"):
-                modulation="QAM %s" % ts["modulation"][3:]
-            elif tuning_params["modulation"] == "reserved":
-                modulation = "QAM 64"
-            else:
-                modulation = ts["modulation"]
-            self.properties.modulation = modulation
+            self.properties.code_rate_hp = self._parseCodeRate(ts["inner-fec"])
+
+    # Private
+
+    def _parseModulation(self, constellation):
+        if constellation.startswith("QAM"):
+            modulation = int(constellation[3:])
+        elif constellation == "reserved":
+            modulation = 64
+        else:
+            #modulation = ts["constellation"]
+            modulation = 64
+        return modulation
+
+    def _parseCodeRate(self, codeRate):
+        if codeRate == "reserved":
+            codeRate = "NONE"
+        return codeRate
+
 
 class DVBAntennaStep(VideoProducerStep):
     name = _('DVB Antenna Selection')
@@ -117,40 +126,57 @@ class DVBAntennaStep(VideoProducerStep):
 
 
     def __init__(self, wizard, model):
-        super(DVBAntennaStep, self).__init__(wizard, model)
         # evil hack to remove need to duplicate getNext code in VideoProducer
         # for last step in producer
         model.firstStep = self
 
+        super(DVBAntennaStep, self).__init__(wizard, model)
+
+    # WizardStep
+
     def workerChanged(self, worker):
         self.model.worker = worker
         self._runChecks()
+
+    def getNext(self):
+        selectedAdapter = self.adapter.get_selected()
+        self.model.adapterNumber = selectedAdapter[1]
+        self.model.properties.adapter = selectedAdapter[1]
+        self.model.dvbtype = selectedAdapter[0]
+        self.model.antenna = self.antenna.get_selected()
+        self.model.properties.dvb_type = self.model.dvbtype[-1]
+        return DVBProbeChannelsStep(self.wizard, self.model)
+
+    # Private
 
     def _runChecks(self):
         msg = messages.Info(T_(N_('Checking for DVB adapters...')),
             id='dvb-adapter-check')
         self.wizard.add_msg(msg)
 
-        d = self.runInWorker('flumotion.component.dvb.dvbchecks', 
-            'getListOfAdaptersWithTypes')
         def adapterListReceived(adapters):
             locationsd = self.runInWorker('flumotion.component.dvb.dvbchecks',
                 'getTerrestrialLocations')
             self._adapters = adapters
             locationsd.addCallback(locationsReceived)
             return locationsd
+
         def locationsReceived(locations):
             self.wizard.clear_msg('dvb-adapter-check')
             self._terrestrialLocations = locations
             self.adapter.prefill([(a[2], a) for a in self._adapters])
+
+        d = self.runInWorker('flumotion.component.dvb.dvbchecks',
+            'getListOfAdaptersWithTypes')
         d.addCallback(adapterListReceived)
-        return d 
-        
-    def on_adapter__changed(self, adaptercombo):
+
+        return d
+
+    def _updateCountries(self):
         # FIXME: add specific code for dvb-s, dvb-c, atsc etc.
         # eg hide country
         # FIXME: get friendly country names
-        adapter = adaptercombo.get_selected()
+        adapter = self.adapter.get_selected()
         if adapter is None:
             return
         dvbType = adapter[0]
@@ -161,24 +187,22 @@ class DVBAntennaStep(VideoProducerStep):
             countries.sort()
             self.country.prefill(countries)
 
-    def on_country__changed(self, countrycombo):
+    def _updateAntenna(self):
         # FIXME: transform camel case titles to have spaces
-        country = countrycombo.get_selected()
+        country = self.country.get_selected()
         if country is None:
             return
         antennae = self._terrestrialLocations[country]
         antennae.sort()
         self.antenna.prefill(antennae)
 
-    # WizardStep
-    def getNext(self):
-        selectedAdapter = self.adapter.get_selected()
-        self.model.adapterNumber = selectedAdapter[1]
-        self.model.properties.adapter = selectedAdapter[1]
-        self.model.dvbtype = selectedAdapter[0]
-        self.model.antenna = self.antenna.get_selected()
-        self.model.properties.dvb_type = self.model.dvbtype[-1]
-        return DVBProbeChannelsStep(self.wizard, self.model)
+    # Callbacks
+
+    def on_adapter__changed(self, adaptercombo):
+        self._updateCountries()
+
+    def on_country__changed(self, countrycombo):
+        self._updateAntenna()
 
 
 class DVBProbeChannelsStep(WizardStep):
@@ -187,52 +211,62 @@ class DVBProbeChannelsStep(WizardStep):
     gladeFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
         'dvb-probe-channels.glade')
     section = _('Production')
-    
+
     def __init__(self, wizard, model):
         self.model = model
+        self._frequenciesToScan = []
+        self._frequenciesAlreadyScanned = []
         super(DVBProbeChannelsStep, self).__init__(wizard)
-        self._frequencies_to_scan = []
-        self._frequencies_already_scanned = []
+
+    # WizardStep
+
+    def setup(self):
         self.model.channels = {}
         self.model.transportStreams = {}
+
         self._runChecks()
+
+    def getNext(self):
+        return DVBSelectChannelStep(self.wizard, self.model)
+
+    # Private API
 
     def _runChecks(self):
         # FIXME: pulse regularly, change label to say what we are doing
         print "Worker %s" % self.model.worker
         print "Adapter %r" % (self.model.adapterNumber,)
         print "Antenna %r" % (self.model.antenna,)
-        d = self.wizard.runInWorker(self.model.worker, 
-            "flumotion.component.dvb.dvbchecks", "getInitialTuning", 
+        d = self.wizard.runInWorker(self.model.worker,
+            "flumotion.component.dvb.dvbchecks", "getInitialTuning",
             self.model.dvbtype, self.model.antenna)
         def receivedInitialTuningData(initialTuning):
             self.progress.pulse()
             print "Initial tuning: %r" % (initialTuning,)
             # now we need to do the scanning
             for freq in initialTuning:
-                self._frequencies_to_scan.append(freq)
+                self._frequenciesToScan.append(freq)
             return self._scan()
         d.addCallback(receivedInitialTuningData)
         return d
 
     def _scan(self):
-        if not self._frequencies_to_scan:
+        if not self._frequenciesToScan:
             print "Finished scanning"
             return
         f = None
         while True:
-            if not self._frequencies_to_scan:
+            if not self._frequenciesToScan:
                 break
-            f = self._frequencies_to_scan.pop()
+            f = self._frequenciesToScan.pop()
             key = f["frequency"], f.get("polarization")
-            if not key in self._frequencies_already_scanned:
+            if not key in self._frequenciesAlreadyScanned:
                 break
             f = None
         if not f:
             print "Finished scanning"
             return
         print "..Frequencies scanned already: %r" % (
-                    self._frequencies_already_scanned,)
+                    self._frequenciesAlreadyScanned,)
         print "Scanning %r" % (f,)
         d = self.wizard.runInWorker(self.worker, "flumotion.component.dvb.dvbchecks",
             "scan", self.model.adapterNumber, self.model.dvbtype, f)
@@ -244,35 +278,22 @@ class DVBProbeChannelsStep(WizardStep):
                 print "Channel %r added with data: %r" % (sid,chanData)
             # add frequency just scanned to frequencies_already_scanned
             key = f["frequency"], f.get('polarization')
-            self._frequencies_already_scanned.append(key)
+            self._frequenciesAlreadyScanned.append(key)
             for tsid, freq in moreFrequencies.items():
                 self.model.transportStreams[tsid] = freq
                 key = freq["frequency"], freq.get('polarization')
-                if key in self._frequencies_already_scanned:
+                if key in self._frequenciesAlreadyScanned:
                     continue
                 print "Delivery: %r" % (freq,)
                 print "Key: %r" % (key,)
                 print "Frequencies scanned already: %r" % (
-                    self._frequencies_already_scanned,)
-                self._frequencies_to_scan.append(freq)
+                    self._frequenciesAlreadyScanned,)
+                self._frequenciesToScan.append(freq)
             print ".Frequencies scanned already: %r" % (
-                    self._frequencies_already_scanned,)
+                    self._frequenciesAlreadyScanned,)
 
             return self._scan()
         d.addCallback(frequencyScanned)
-        
-    def getNext(self):
-        return DVBSelectChannelStep(self.wizard, self.model)
-
-# for kiwi ObjectList
-class DVBChannel:
-    def __init__(self, sid, chandata):
-        self.service_id = sid
-        self.name = chandata.get('name', 'Unknown')
-        self.chandata = chandata
-    
-    def getTransportStreamId(self):
-        return self.chandata["transport-stream-id"]
 
 
 class DVBSelectChannelStep(WizardStep):
@@ -286,9 +307,11 @@ class DVBSelectChannelStep(WizardStep):
         self.model = model
         super(DVBSelectChannelStep, self).__init__(wizard)
 
+    # WizardStep
+
     def setup(self):
         self.channels = ObjectList([
-            Column('service_id', title='Program number'), 
+            Column('service_id', title='Program number'),
             Column('name', title='Channel name', sorted=True)])
         for sid,chandata in self.model.channels.items():
             self.channels.append(DVBChannel(sid, chandata))
@@ -301,8 +324,9 @@ class DVBSelectChannelStep(WizardStep):
         self.model.channel = channel
         self.model.properties.program_number = channel.service_id
         tsid = channel.getTransportStreamId()
-        self.model.setTuningInformation(tsid)        
+        self.model.setTuningInformation(tsid)
         return DVBVideoConfig(self.wizard, self.model)
+
 
 class DVBVideoConfig(WizardStep):
     name = _("Video Configuration")
@@ -316,6 +340,8 @@ class DVBVideoConfig(WizardStep):
         self.model = model
         super(DVBVideoConfig, self).__init__(wizard)
 
+    # WizardStep
+
     def setup(self):
         self.framerate.data_type = float
 
@@ -325,13 +351,14 @@ class DVBVideoConfig(WizardStep):
     def getNext(self):
         return VideoProducerStep.getNext(self.model.firstStep)
 
+
 class DVBWizardPlugin(object):
     implements(IProducerPlugin)
 
     def __init__(self, wizard):
         self.wizard = wizard
         self.model = DVBProducer()
-    
+
     def getProductionStep(self, type):
         return DVBAntennaStep(self.wizard, self.model)
 
